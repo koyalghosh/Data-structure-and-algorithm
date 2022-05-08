39e55ba58545f24378e3e5b6fae73d30a47cd252
koyalghosh <80525025+koyalghosh@users.noreply.github.com>
2022-05-08 22:47:18 +0530

    Update selection.cpp

--- Sorting/selection.cpp
+++ Sorting/selection.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int *ar;
+	int n,i,j;
+	cout<<"Enter number of elements:- ";
+	cin>>n;
+	ar=new int [n];
+	//input
+	for(i=0;i<n;i++){
+		cout<<"Enter "<<i+1<<"element : ";
+		cin>>ar[i];
+	}
+	//selection sort loop
+	for(i=0;i<n-1;i++){
+		for(j=i+1;j<n;j++){
+			if(ar[i]>ar[j]){
+				int t;
+				t=ar[i];
+				ar[i]=ar[j];
+				ar[j]=t;
+			}
+		}
+	}
+	//printing 
+	for(int i=0;i<n;i++)
+		cout<<ar[i]<<"\t";
+	return 0;
+}
 
